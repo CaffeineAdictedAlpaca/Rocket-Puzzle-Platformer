a6a15292a2eb5b14a2a78dfeea65f96089e6fa61
Mxtern <max.toresjo@elev.ga.lbs.se>
2024-02-20 15:10:41 +0100

    explotion

---
+++ Assets/Scripts/explotion.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explotion : MonoBehaviour
+{
+    public GameObject test;
+    [SerializeField, Range(0.5f, 100)] float maxRadius;
+    RaycastHit rocketHit;
+    [SerializeField] LayerMask mask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray lineOfSight = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            if (Physics.Raycast(lineOfSight,out rocketHit))
+            {
+                print(rocketHit.point);
+                Instantiate(test, rocketHit.point, Quaternion.identity);
+               Collider[] objNear = Physics.OverlapSphere(rocketHit.point, maxRadius, mask);
+                for (int i = 0; i < objNear.Length; i++)
+                {
+                  Destroy(objNear[i].gameObject);
+                }
+            }
+        }
+       
+    }
+}
