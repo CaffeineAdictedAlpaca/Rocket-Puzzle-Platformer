a83907c5eea85c960a46e6325bb4236290420891
CaffeineAdictedAlpaca <119925733+CaffeineAdictedAlpaca@users.noreply.github.com>
2024-04-10 09:20:55 +0200

    Button!

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Button : Interact
 {
-    [SerializeField] bool NegativeY;
-    [SerializeField] bool NegativeX;
-    [SerializeField] bool Y;
-    [SerializeField] bool X;
-
+    [Header("Adjustment")]
     [SerializeField] float travel;
     [SerializeField] float time;
 
     Vector3 destination;
     Vector3 origin;
+
+    public bool activated;
+
+    [SerializeField] Material green;
 
     public override void Start()
     {
@@ -24,33 +24,34 @@ public class Button : Interact
     // Update is called once per frame
     public override void _Interact()
     {
-        if (NegativeX)
-        {
-            destination = new Vector3(transform.position.x - travel, transform.position.y, transform.position.z);
-            StartCoroutine(moveObject());
-        }
+        destination = transform.position + transform.up * travel;
+        StartCoroutine(MoveObject());
     }
-    public IEnumerator moveObject()
+    public IEnumerator MoveObject()
     {
         float totalMovementTime = time; //the amount of time you want the movement to take
         float currentMovementTime = 0f;//The amount of time that has passed
         while (Vector3.Distance(transform.localPosition, destination) > 0)
         {
+            canInteract = false;
             currentMovementTime += Time.deltaTime;
             transform.localPosition = Vector3.Lerp(origin, destination, currentMovementTime / totalMovementTime);
-            StartCoroutine(moveObject2());
             yield return null;
         }
+        activated = true;
+        gameObject.GetComponent<Renderer>().material = new Material(green);
+        StartCoroutine(MoveObject2());
     }
-    public IEnumerator moveObject2()
+    public IEnumerator MoveObject2()
     {
         float totalMovementTime = time; //the amount of time you want the movement to take
         float currentMovementTime = 0f;//The amount of time that has passed
-        while (Vector3.Distance(transform.localPosition, destination) > 0)
+        while (Vector3.Distance(transform.localPosition, origin) > 0)
         {
             currentMovementTime += Time.deltaTime;
             transform.localPosition = Vector3.Lerp(destination, origin, currentMovementTime / totalMovementTime);
             yield return null;
         }
+        canInteract = true;
     }
 }
